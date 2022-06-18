dce510d5ff90ebee3ca207ada609def92f07948c
Ceazar John Drio <cjzar12@gmail.com>
2022-06-18 20:00:52 +0800

    FINALIZED THE APP

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,2 +1,3 @@
 export const AUTH_DOMAIN = "dev-9h67ppj9.us.auth0.com"
 export const AUTH_CLIENT_ID = "sTNtPW4ZITMyYYuoSVdRIPOzAGnIKCNW"
+export const WEATHER_API_APPID = "3f552caae102e1528070cda1da64374c"
